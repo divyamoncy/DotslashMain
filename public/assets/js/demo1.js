34ab1173f5c73ba8381628403973c04ac8736b5e
Jilvin <jilvinjacob@gmail.com>
2018-02-15 22:05:46 +0530

    added reg fee for bitcoin job

--- public/assets/js/demo1.js
+++ public/assets/js/demo1.js
@@ -1,8 +1,8 @@
 (function(){
 
 	var button = document.getElementById('cn-button'),
-    wrapper = document.getElementById('cn-wrapper'),
-    overlay = document.getElementById('cn-overlay');
+	wrapper = document.getElementById('cn-wrapper'),
+	overlay = document.getElementById('cn-overlay');
 
 	//open and close menu when the button is clicked
 	var open = false;
@@ -15,21 +15,20 @@
 
 	function handler(e){
 		if (!e) var e = window.event;
-	 	e.stopPropagation();//so that it doesn't trigger click event on document
+		e.stopPropagation();//so that it doesn't trigger click event on document
 
-	  	if(!open){
-	    	openNav();
-	  	}
-	 	else{
-	    	closeNav();
-
-	  	}
+		if(!open){
+			openNav();
+		}
+		else{
+			closeNav();
+		}
 	}
 	function openNav(){
 		open = true;
-	    button.innerHTML = "-";
-	    classie.add(overlay, 'on-overlay');
-	    classie.add(wrapper, 'opened-nav');
+		button.innerHTML = "-";
+		classie.add(overlay, 'on-overlay');
+		classie.add(wrapper, 'opened-nav');
 	}
 	function closeNav(){
 		open = false;
@@ -37,7 +36,5 @@
 		classie.remove(overlay, 'on-overlay');
 		classie.remove(wrapper, 'opened-nav');
 	}
-	 document.addEventListener('click', closeNav);
-
+	document.addEventListener('click', closeNav);
 })();
-
