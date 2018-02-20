b18540ca5599e7054fc8ea55406a280a9a2d6672
Jilvin <jilvinjacob@gmail.com>
2018-02-20 11:33:35 +0530

    added open letter

---
+++ public/open_letter/email.js
@@ -0,0 +1,18 @@
+<SCRIPT TYPE="text/javascript">
+<!--
+// protected email script by Joe Maller
+// JavaScripts available at http://www.joemaller.com
+// this script is free to use and distribute
+// but please credit me and/or link to my site
+
+emailE='cet.ac.in'
+emailE=('dotslash' + '@' + emailE)
+document.write('<A href="mailto:' + emailE + '">' + emailE + '</a>')
+
+//-->
+</script>
+
+<NOSCRIPT>
+<em>Email address protected by JavaScript.<BR>
+Please enable JavaScript to contact me.</em>
+</NOSCRIPT>
